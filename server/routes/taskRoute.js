4b4320ed00ccde5a268dd50c04cfbd610ada3c69
NAVEENKUMAR K <86141832+knk-naviin@users.noreply.github.com>
2024-05-21 20:05:01 +0530

    committ

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,18 +1,24 @@
-const express = require('express')
+const express = require("express");
 // to import router from EXPRESS JS
-const router = express.Router()
+const router = express.Router();
 
 //Then call back controller from taskcontroller
 
-const {createTask,getTasks,getSingleTask,updateTask,delTask} = require("../controller/taskController");
+const {
+  createTask,
+  getTasks,
+  getSingleTask,
+  updateTask,
+  delTask,
+} = require("../controller/taskController");
 
 // passing the argument from controller to route by POST method
 
-router.post('/',createTask);
-router.get('/',getTasks);
-router.get('/:id',getSingleTask);
-router.patch('/:id',updateTask);
-router.delete('/:id',delTask);
+router.post("/", createTask);
+router.get("/", getTasks);
+router.get("/:id", getSingleTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", delTask);
 
 //exporting module from routes
 
--- server/server.js
+++ server/server.js
@@ -1,37 +1,41 @@
 //import of NPM packages
 
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
-const taskroutes = require('./routes/taskRoute');
+const taskroutes = require("./routes/taskRoute");
 
 // MiddleWare
-app.use((req,res,next) =>{
-    console.log("path  " + req.path + "Method   " + req.method + "  " + res.method);
-    next();
+app.use((req, res, next) => {
+  console.log(
+    "path  " + req.path + "Method   " + req.method + "  " + res.method
+  );
+  next();
 });
 
 app.use(express.json());
 
 //Methods GET,POST,PATCH,DELETE
 
-app.get("/",(req,res)=>{
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
 //DB Connection
 
-mongoose.connect(process.env.MONGO_URI)
-//we establish that db connection , where it will enable the db connected then it will show the PORT
-   .then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("DB Connected Succesfully & Listening to port " + process.env.PORT);
+mongoose
+  .connect(process.env.MONGO_URI)
+  //we establish that db connection , where it will enable the db connected then it will show the PORT
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "DB Connected Succesfully & Listening to port " + process.env.PORT
+      );
     });
-    
-   }).catch((error) =>{
+  })
+  .catch((error) => {
     console.log(error);
-   });
+  });
 
-
-   app.use("/api/tasks",taskroutes);
+app.use("/api/tasks", taskroutes);
